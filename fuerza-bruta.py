b5a3c742d0ac1b64cbcf703d2f170a6041f551a0
rodriguinpinpin <amner.cabada@gmail.com>
2026-06-29 16:19:53 -0500

    Agregamos unfor para que se lea el peso y el valor del objeto

--- fuerza-bruta.py
+++ fuerza-bruta.py
@@ -4,3 +4,7 @@ def mochila_fuerza_bruta():
     
     pesos = [0] * n
     valores = [0] * n
+# Leer datos de cada objeto
+    for i in range(n):
+        pesos[i] = int(input(f"Peso del objeto {i + 1}: "))
+        valores[i] = int(input(f"Valor del objeto {i + 1}: "))    
