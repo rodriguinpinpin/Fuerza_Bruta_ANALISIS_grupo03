34ff1dbcef39cdac8df0a99c0589b4547369708b
rodriguinpinpin <amner.cabada@gmail.com>
2026-06-29 16:21:23 -0500

    Ahora insertamos para que se lea la capacidad de la mochila

--- fuerza-bruta.py
+++ fuerza-bruta.py
@@ -4,7 +4,14 @@ def mochila_fuerza_bruta():
     
     pesos = [0] * n
     valores = [0] * n
+    
 # Leer datos de cada objeto
     for i in range(n):
         pesos[i] = int(input(f"Peso del objeto {i + 1}: "))
-        valores[i] = int(input(f"Valor del objeto {i + 1}: "))    
+        valores[i] = int(input(f"Valor del objeto {i + 1}: "))  
+          
+# Leer capacidad de la mochila
+    capacidad = int(input("Capacidad de la mochila: "))
+    
+    peso_total = 0
+    valor_total = 0        
